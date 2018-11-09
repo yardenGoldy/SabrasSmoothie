d80817932514080272c81ebd3ec36933a4727476
Ofri Peretz <ofri95@gmail.com>
2018-11-10 00:57:15 +0200

    Set Register Page & redirect to login right after

--- SabrasSmoothie/Controllers/RegisterController.cs
+++ SabrasSmoothie/Controllers/RegisterController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SabrasSmoothie.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,34 @@ namespace SabrasSmoothie.Controllers
 {
     public class RegisterController : Controller
     {
+        private CustomerDbContext db = new CustomerDbContext();
+
         public ActionResult Index()
         {
-            ViewBag.Message = "Our application registration page.";
             return View();
+        }
+
+        // GET: Register/Create
+        public ActionResult Success()
+        {
+            return View();
+        }
+
+        // POST: Register/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "Id,FirstName,LastName,BirthDate,UserName,Password,Address,City,ZipCode")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                customer.IsAdmin = false;
+
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Success");
+            }
+
+            return View(customer);
         }
     }
 }
