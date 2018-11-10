b65687e720991eee45f8fff68898731842498df8
Yuval Aharoni <yuvalaharoni585@gmail.com>
2018-11-11 00:28:28 +0200

    Auto stash before merge of "AuthCheck" and "origin/master"

--- SabrasSmoothie/Controllers/HomeController.cs
+++ SabrasSmoothie/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
-﻿using SabrasSmoothie.Models;
+﻿using Newtonsoft.Json.Linq;
+using SabrasSmoothie.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,8 +13,15 @@ namespace SabrasSmoothie.Controllers
     public class HomeController : Controller
     {
         private ProductDbContext Product = new ProductDbContext();
+        private CustomerDbContext _Customer = new CustomerDbContext();
+
         public ActionResult Index()
         {
+            // Debugging Mode! Delete In Production!
+            var user = _Customer.Customers.SingleOrDefault(customer => customer.UserName == "Admin" && customer.Password == "Admin");
+            Session["Admin"] = true;
+
+
             var products = Product.SortByOrders();
 
             var productsByCalories = Product.GroupByCalories();
@@ -31,6 +41,35 @@ namespace SabrasSmoothie.Controllers
 
             ViewBag.Products = products;
             return View();
+        }
+
+        public static void DupPrices(Product[] products) {
+            
+            JObject Answer = JObject.Parse(GetCurrency("http://data.fixer.io/api/latest?access_key=ae8df3ea35ce6a2a4c670b1b6407a86d&symbols=USD,ILS&format=1"));
+
+            int currToCalc = (int)float.Parse(Answer["rates"]["USD"].ToString());
+            //if (Session["Currency"] == null)
+            //{
+            //    currToCalc = (int)float.Parse(Answer["rates"]["ILS"].ToString());
+            //}
+
+            foreach (Product product in products)
+            {
+                product.Price *= currToCalc;
+            }
+        }
+
+        public static string GetCurrency(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
         }
     }
 }
